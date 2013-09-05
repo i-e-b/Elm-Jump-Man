55fb68a12f0c4dbcc91dc57fe098bccd90164b44
Iain Ballard <iain.ballard@gmail.com>
2013-09-05 13:30:25 +0100

    block drawing, Y align needs to be better

--- Main.elm
+++ Main.elm
@@ -3,21 +3,25 @@ import Window
 import Dict
 
 -- Game entities
-type Mario = {x:Float, y:Float, vx:Float, vy:Float, dir:String, jumpFrames:Int}
+type Mario = {x:Float, y:Float, vx:Float, vy:Float, dir:String, jumpEnergy:Int}
 type Controls = (Float, {x:Int, y:Int})
 
 -- First try at level structure: a Dict of x locations, each with a list of floor heights.
 -- Later, this should load levels over HTTP, and be a signal going into the game.
--- Worlds are blocky, and each position relates to 20.0 pixels position
+-- Worlds are blocky, and each position relates to a block index.
+-- All blocks are the same size (blockScale)
 type World = Dict.Dict Int [Int]
 world = Dict.fromList [(0,[3,1]),(1,[2])]
+blockScale = 16.0
+halfBlockScale = 8.0
 
 -- Structural classes
 type Positional a = {a | x:Float, y:Float}
 
 -- MODEL
+maxJump = 7
 mario : Mario
-mario = { x=0, y=0, vx=0, vy=0, dir="right", jumpFrames=0}
+mario = { x=0, y=0, vx=0, vy=0, dir="right", jumpEnergy=maxJump}
 
 {- To do:
  - blocks & floors, jumping
@@ -35,26 +39,24 @@ onSurface m = (m.y <= floorLevel m)
 -- nearest floor below us
 floorLevel : Mario -> Float
 floorLevel m =
-    let blockX = truncate (m.x / 20)
-        heights = map (\h->h*20) (Dict.findWithDefault [] blockX world) ++ [0]
+    let blockX = truncate (m.x / blockScale)
+        heights = map (\h->h*blockScale) (Dict.findWithDefault [] blockX world) ++ [0]
     in  head (filter (\h -> h <= m.y || h == 0) heights)
-{-    let floor = [
-    in  if
-        | (nearEnough m ()) -> 20
-        | otherwise -> 0
--}
 
 nearEnough : Positional a -> Positional b -> Bool
-nearEnough u v = abs (u.x - v.x) < 10 && abs (u.y - v.y) < 10
+nearEnough u v = abs (u.x - v.x) < halfBlockScale && abs (u.y - v.y) < halfBlockScale
 
 -- should be able to jump when on a surface (or enemy)
 -- can moderate height of jump by duration of 'up' press
-jump {y} m = if y > 0 && (onSurface m || m.jumpFrames < 7) then { m | vy <- 4, jumpFrames <- m.jumpFrames + 1 } else m
+jump {y} m = if
+    | y > 0 && (onSurface m || m.jumpEnergy > 0) -> { m | vy <- 4, jumpEnergy <- m.jumpEnergy - 1 }
+    | y <= 0 && not (onSurface m) -> {m | jumpEnergy <- 0}
+    | otherwise -> m
 
--- gravity needs to stop on surface rather than zero
-gravity t m = if not (onSurface m) then { m | vy <- m.vy - t/4 } else {m | jumpFrames <- 0}
+-- fall unless sitting on a surface
+gravity t m = if not (onSurface m) then { m | vy <- m.vy - t/4 } else {m | jumpEnergy <- maxJump}
 
--- these two are probably ok for now.
+-- apply acceleration and constraints
 physics t m = { m | x <- m.x + t*m.vx , y <- max (floorLevel m) (m.y + t*m.vy) }
 walk {x} m = { m | vx <- toFloat x
                  , dir <- if | x < 0     -> "left"
@@ -74,14 +76,29 @@ render (w',h') mario =
                 | mario.vx /= 0 -> "walk"
                 | otherwise     -> "stand"
       src  = "/imgs/mario/" ++ verb ++ "-" ++ mario.dir ++ ".gif"
-  in collage w' h'
-      [ rect w h  |> filled (rgb 174 238 238)
-      , rect w 50 |> filled (rgb 74 163 41)
-                  |> move (0, 24 - h/2)
-      , toForm (image 35 35 src) |> move (mario.x, mario.y + 62 - h/2)
-      ]
+  in collage w' h' (
+        renderBackground (w,h) world
+        ++
+        [toForm (image 35 35 src) |> move (mario.x, mario.y + 62 - h/2)]
+        ++
+        renderBlocks (w,h) world
+        )
 
--- MARIO
+renderBlocks : (Float, Float) -> World -> [Form]
+renderBlocks (w',h') w = {- hard coded to start -}
+    let block x y = toForm (image 16 16 "/imgs/world/smash-block.png") 
+                    |> move ((blockScale * x), (blockScale * y) + 40 - h'/2)
+        allBlocks = concatMap (\(x, ys) -> map (\y -> block (toFloat x) (toFloat y)) ys) (Dict.toList w)
+    in  allBlocks
+
+renderBackground : (Float,Float) -> World -> [Form]
+renderBackground (w,h) wr = 
+    [ rect w h  |> filled (rgb 174 238 238)
+    , rect w 50 |> filled (rgb 74 163 41)
+                  |> move (0, 24 - h/2)
+    ]
+
+-- Hooking everything up...
 input : Signal Controls
 input = let delta = lift (\t -> t/20) (fps 25)
         in sampleOn delta (lift2 (,) delta Keyboard.arrows)
