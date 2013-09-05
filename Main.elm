f750e2d5495624ba1ef35c67d8cb0643c1f2ebf5
i-e-b <ieb@me.com>
2013-09-05 01:19:34 +0100

    Can haz invisible block

--- Main.elm
+++ Main.elm
@@ -3,10 +3,17 @@ import Window
 
 type Mario = {x:Float, y:Float, vx:Float, vy:Float, dir:String, jumpFrames:Int}
 type Controls = (Float, {x:Int, y:Int})
+type Block = {x:Float, y:Float}
+type Wall = [Block]
+
+type Positional a = {a | x:Float, y:Float}
 
 -- MODEL
 mario : Mario
 mario = { x=0, y=0, vx=0, vy=0, dir="right", jumpFrames=0}
+
+world : Wall
+world = [{x=0, y=4}]
 
 {- To do:
  - blocks & floors, jumping
@@ -19,17 +26,27 @@ mario = { x=0, y=0, vx=0, vy=0, dir="right", jumpFrames=0}
 
 -- are we standing on a surface? If not, we are in free fall (possibly jumping)
 onSurface : Mario -> Bool
-onSurface m = m.y <= 0
+onSurface m = (m.y <= floorLevel m)
+
+-- nearest floor below us
+floorLevel : Mario -> Float
+floorLevel m = if 
+    | (nearEnough m {x=0, y=20}) -> 20
+    | otherwise -> 0
+
+
+nearEnough : Positional a -> Positional b -> Bool
+nearEnough u v = abs (u.x - v.x) < 10 && abs (u.y - v.y) < 10
 
 -- should be able to jump when on a surface (or enemy)
 -- can moderate height of jump by duration of 'up' press
-jump {y} m = if y > 0 && (m.y == 0 || m.jumpFrames < 7) then { m | vy <- 4, jumpFrames <- m.jumpFrames + 1 } else m
+jump {y} m = if y > 0 && (onSurface m || m.jumpFrames < 7) then { m | vy <- 4, jumpFrames <- m.jumpFrames + 1 } else m
 
 -- gravity needs to stop on surface rather than zero
 gravity t m = if not (onSurface m) then { m | vy <- m.vy - t/4 } else {m | jumpFrames <- 0}
 
 -- these two are probably ok for now.
-physics t m = { m | x <- m.x + t*m.vx , y <- max 0 (m.y + t*m.vy) }
+physics t m = { m | x <- m.x + t*m.vx , y <- max (floorLevel m) (m.y + t*m.vy) }
 walk {x} m = { m | vx <- toFloat x
                  , dir <- if | x < 0     -> "left"
                              | x > 0     -> "right"
@@ -44,7 +61,7 @@ step (t,dir) = physics t . walk dir . gravity t . jump dir
 render : (Int, Int) -> Mario -> Element 
 render (w',h') mario =
   let (w,h) = (toFloat w', toFloat h')
-      verb = if | mario.y  >  0 -> "jump"
+      verb = if | mario.y  >  floorLevel mario -> "jump"
                 | mario.vx /= 0 -> "walk"
                 | otherwise     -> "stand"
       src  = "/imgs/mario/" ++ verb ++ "-" ++ mario.dir ++ ".gif"
